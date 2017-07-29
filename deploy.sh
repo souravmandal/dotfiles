bedf16e8d3e68073badcc06979b453dcb2080f1a
Sourav K. Mandal <sourav.mandal@gmail.com>
2017-07-29 12:56:35 -0400

    Changes to be committed: 	modified:   ../../.gitmodules 	modified:   ../../deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-dotfiles=$(ls -a | grep -v git | grep '^\.[^\.$]')
+dotfiles=$(ls $HOME/dotfiles -a | grep -v git | grep '^\.[^\.$]')
 
 for df in $dotfiles; do
     echo "Deploying $df"
